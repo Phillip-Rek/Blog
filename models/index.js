1a4d538f37dcc6c89d1286f30b106af7ecf169c3
phillip-rek <philliprekhotho@gmail.com>
2021-01-21 11:46:27 +0200

    fix: system is down

--- models/index.js
+++ models/index.js
@@ -9,7 +9,7 @@
 //     uri: "postgres://oxxezrmpkxzyus:63787b3172ce6501ddb4a91f564dff4702565d53649e2b933f5c396077b910f6@ec2-54-144-45-5.compute-1.amazonaws.com:5432/dc3g6b2jpvldfk"
 // })
 
-const Pool = require('pg').Pool
+const { Pool } = require('pg').Pool
 
 const pool = new Pool({
     host: "ec2-54-144-45-5.compute-1.amazonaws.com",
@@ -20,11 +20,12 @@ const pool = new Pool({
 })
 
 pool.connect((e) => {
-    return e ? console.log("cannot connect to the database " + e) :
+    if (e) {
+        console.log("cannot connect to the database " + e)
+    } else {
         console.log("connected to the database")
-});
-
-
+    }
+})
 
 module.exports = {};
 module.exports.pool = pool;
