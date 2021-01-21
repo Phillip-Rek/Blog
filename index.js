2289ec35553f9f5298bf64fd5e9541ee4a5ee40e
phillip-rek <philliprekhotho@gmail.com>
2021-01-21 02:23:03 +0200

    adding port

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 //const Articles = require("./models/articles");
 
+let port = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
     req.send("<h1>hello world</h1>")
 })
